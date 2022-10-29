50dfe835247471cc233903e8a636681d6f4b0a28
Ruben Nuñez <ruben.nunez@live.de>
2022-10-29 12:17:32 +0200

    country deselect filter

--- src/newsmap-frontend/src/news/News.tsx
+++ src/newsmap-frontend/src/news/News.tsx
@@ -5,29 +5,33 @@ import { NewsGlobe } from "./NewsGlobe";
 import { NewsTimeline } from "./NewsTimeline";
 
 export function News() {
-    const [countries, setCountries] = useState([] as ICountry[]);
+  const [countries, setCountries] = useState([] as ICountry[]);
 
-    useEffect(() => {
-      fetch('./countries.json').then(res => res.json())
-        .then((c) => setCountries(c));
-    }, []);
+  useEffect(() => {
+    fetch('./countries.json').then(res => res.json())
+      .then((c) => setCountries(c));
+  }, []);
 
-    
-    const [news, setNews] = useState([] as INews[]);
 
-    useEffect(() => {
-      fetch('./newsdata.json').then(res => res.json())
-        .then((n) => setNews(n));
-    }, []);
+  const [news, setNews] = useState([] as INews[]);
 
-    const [selectedCountry, setSelectedCountry] = useState(undefined as ICountry | undefined);
-    const [hoveredNews, setHoveredNews] = useState(undefined as INews | undefined);
+  useEffect(() => {
+    fetch('./newsdata.json').then(res => res.json())
+      .then((n) => setNews(n));
+  }, []);
 
-  
+  const [selectedCountry, setSelectedCountry] = useState(undefined as ICountry | undefined);
+  const [hoveredNews, setHoveredNews] = useState(undefined as INews | undefined);
+
+
   return (
     <>
-      <NewsTimeline countries={countries} news={news} countryFilter={selectedCountry} onHover={setHoveredNews}/>
-      <NewsGlobe countries={countries} news={news} countryFilter={selectedCountry} onCountryClick={setSelectedCountry} hoveredNews={hoveredNews}/>
+      <NewsTimeline countries={countries}
+        news={news}
+        countryFilter={selectedCountry}
+        onSelectedCountry={setSelectedCountry}
+        onHover={setHoveredNews} />
+      <NewsGlobe countries={countries} news={news} countryFilter={selectedCountry} onCountryClick={setSelectedCountry} hoveredNews={hoveredNews} />
     </>
   );
 }
--- src/newsmap-frontend/src/news/NewsTimeline.tsx
+++ src/newsmap-frontend/src/news/NewsTimeline.tsx
@@ -1,41 +1,57 @@
 import { INews } from "../interfaces/INews";
 import './NewsTimline.css'
 
-import { useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { ICountry } from "../interfaces/ICountry";
 
 export interface INewsTimelineProps {
     news: INews[];
     countries: ICountry[];
     countryFilter: ICountry | undefined;
+    onSelectedCountry: (country: ICountry | undefined) => void;
     onHover: (news: INews | undefined) => void;
-  }
 
-export function NewsTimeline(props: INewsTimelineProps){
-    
+}
+
+export function NewsTimeline(props: INewsTimelineProps) {
+
     let scrollContainer = useRef<HTMLDivElement>(null);
 
-    let filteredNews = useMemo(() => props.news.filter((n) => props.countryFilter ? n.country === props.countryFilter.cca2 : true),[props.countryFilter,props.news]);
+    let filteredNews = useMemo(() => props.news.filter((n) => props.countryFilter ? n.country === props.countryFilter.cca2 : true), [props.countryFilter, props.news]);
+
+    let onCountryFilterClear = () => {
+        props.onSelectedCountry(undefined);
+    };
+
+    useEffect(() => {
+        if (!props.countryFilter){
+            scrollContainer.current?.scrollTo(0, 0);            
+        }
+    }, [props.countryFilter]);
 
     let elements = filteredNews.map((news) => {
-        return <div key={news._id} className="scroll-item" onMouseOver={() => props.onHover(news)} onMouseLeave={() => props.onHover(undefined)}>              <p className="headline">{news.title}</p>
-                <hr className="line"/>
-                <p className="subline">{news.country + " - " + news.rights + " - " + new Date(news.published_date).toLocaleString('de-CH', {
-  month: 'long', // "June"
-  day: '2-digit', // "01"
-  year: 'numeric' // "2019"
-})}</p>
-        </div>;
+        return <div key={news._id} className="scroll-item" onMouseOver={() => props.onHover(news)} onMouseLeave={() => props.onHover(undefined)}>
+            <p className="headline">{news.title}</p>
+            <hr className="line" />
+            <p className="subline">{news.country + " - " + news.rights + " - " + new Date(news.published_date).toLocaleString('de-CH', {
+                month: 'long', // "June"
+                day: '2-digit', // "01"
+                year: 'numeric' // "2019"
+            })}
+            </p>
+        </div>
     });
 
     return <div className="timeline-wrapper" >
-        <div ref={scrollContainer} 
-             className='scroll-container'> 
-             <h1 className="title">Newsmap</h1> 
-                {elements}
+        <div ref={scrollContainer}
+            className='scroll-container'>
+            <h1 className="title">Newsmap</h1>
             <div className="chips-container">
-    <p>TEST</p>
+                {
+                    props.countryFilter ?  <div className="chips" onClick={()=>onCountryFilterClear()}>{props.countryFilter.name.common} 🗑</div> : <></>
+                }
             </div>
+            {elements}
         </div>
-    </div> 
+    </div>
 }
