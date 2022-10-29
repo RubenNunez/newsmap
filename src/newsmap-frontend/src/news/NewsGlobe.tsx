0fb0f536a151e98339b056dc3c0d83e167a1c681
Dario Erismann <dario.erismann@hotmail.com>
2022-10-29 11:21:30 +0200

    focus news

--- src/newsmap-frontend/src/news/NewsGlobe.tsx
+++ src/newsmap-frontend/src/news/NewsGlobe.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import Globe from "react-globe.gl";
 import { ICountry } from "../interfaces/ICountry";
 import { INews } from "../interfaces/INews";
@@ -31,6 +31,8 @@ function getSize(newsCountry: INewsCountry) {
   return size < 1 ? 1 : size;
 }
 
+
+
 export function NewsGlobe(props: INewsGlobeProps) {
   let groupedNews = useMemo(() => {
     let g = groupBy(props.news, (item) => item.country);
@@ -41,11 +43,28 @@ export function NewsGlobe(props: INewsGlobeProps) {
     return groupedNews;
   }, [groupedNews]);
 
+  let globeElement = useRef<any>(null);
+
+  useEffect(() => {
+    if(props.hoveredNews){
+      let country =  props.news.find((nw: any) => nw._id === props.hoveredNews?._id)?.country;
+      let coords = props.countries.find((c) => c.cca2.toUpperCase() === country?.toUpperCase())?.latlng;
+      if(coords){
+        globeElement.current?.pointOfView({ lat: coords[0], lng: coords[1],altitude: 2 },1000);
+
+      }
+
+
+    }
+
+  }, [props.hoveredNews]);
+
   return !props.news ? (
     <div></div>
   ) : (
     <div className="globe-wrapper">
     <Globe
+      ref={globeElement}
       backgroundColor={"#ffffff"}
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
       labelsData={fiteredNews}
@@ -56,7 +75,7 @@ export function NewsGlobe(props: INewsGlobeProps) {
       labelText={(n: any) => n.country.name.common}
       labelSize={(n: any) =>  getSize(n)}
       labelDotRadius={(n: any) => getSize(n)}
-      labelColor={() => "rgba(255, 165, 0, 0.75)"}
+      labelColor={(n: any) => props.hoveredNews && n.news.find((nw: any) => nw._id === props.hoveredNews?._id) ? "#ff0000" : "rgba(255, 165, 0, 0.75)"}
       onLabelClick={(n: any) => {
         props.onCountryClick(n.country);
       }}
--- src/newsmap-frontend/src/news/NewsTimeline.tsx
+++ src/newsmap-frontend/src/news/NewsTimeline.tsx
@@ -1,22 +1,24 @@
 import { INews } from "../interfaces/INews";
 import './NewsTimline.css'
 
-import { useRef } from "react";
+import { useRef, useMemo } from "react";
 import { ICountry } from "../interfaces/ICountry";
 
 export interface INewsTimelineProps {
     news: INews[];
     countries: ICountry[];
     countryFilter: ICountry | undefined;
-    onHover: (news: INews) => void;
+    onHover: (news: INews | undefined) => void;
   }
 
 export function NewsTimeline(props: INewsTimelineProps){
     
     let scrollContainer = useRef<HTMLDivElement>(null);
 
-    let elements = props.news.map((news) => {
-        return <div key={news._id} className="scroll-item">
+    let filteredNews = useMemo(() => props.news.filter((n) => props.countryFilter ? n.country === props.countryFilter.cca2 : true),[props.countryFilter,props.news]);
+
+    let elements = filteredNews.map((news) => {
+        return <div key={news._id} className="scroll-item" onMouseOver={() => props.onHover(news)} onMouseLeave={() => props.onHover(undefined)}>
                 <p className="headline">{news.title}</p>
         </div>;
     });
